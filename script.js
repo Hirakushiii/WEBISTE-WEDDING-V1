fc86fdcc48735d04ac8104530c25fe6d862633e7
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-03-28 23:32:13 +0700

    Menambahkan sedikit fitur termasuk disable scrooling

---
+++ script.js
@@ -0,0 +1,23 @@
+function EnableScrool(){
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    
+    window.onscroll = () =>{
+        window.scrollTo(scrollTop, scrollLeft);
+    }
+
+    document.querySelector(":root").style.scrollBehavior = 'auto';
+}
+function DisableScrool() {
+    alert('Selamat Datang!')
+    window.onscroll = () =>{}
+    document.querySelector(":root").style.scrollBehavior = 'smooth';
+
+    localStorage.setItem('opened', 'true');
+    document.querySelector('#main-btn').classList.add('d-none');
+}
+if(!localStorage.getItem('opened')){
+    EnableScrool();
+}else{
+    document.querySelector('#main-btn').classList.add('d-none');
+}
