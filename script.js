d3d25305614c30c6ced535f528f7c08ffcf81f60
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-03-31 15:17:22 +0700

    Menambahkan fitur musik in page dan lain-lain

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const song = document.querySelector('#song');
+const DiskOn = document.querySelector('#disk-on');
+const DiskIcon = document.querySelector('#disk-on i');
+let SongOnPlay = false;
+
 function EnableScrool(){
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
@@ -12,12 +17,35 @@ function DisableScrool() {
     alert('Selamat Datang! \n Silahkan cek waktu dan tempat berlangsungnya acara \n Beserta reservasi kedatangan');
     window.onscroll = () =>{}
     document.querySelector(":root").style.scrollBehavior = 'smooth';
-
+    document.querySelector('#main-btn').setAttribute('href', '#home');
     localStorage.setItem('opened', 'true');
     document.querySelector('#main-btn').classList.add('d-none');
+    Playaudio();
 }
-if(!localStorage.getItem('opened')){
-    EnableScrool();
-}else{
-    document.querySelector('#main-btn').classList.add('d-none');
+function Playaudio(){
+    DiskOn.style.display = 'flex';
+    song.play();
+    song.volume = 0.3;
+    SongOnPlay = true;
 }
+DiskOn.addEventListener('click', () =>{
+    if(SongOnPlay){
+        song.pause();
+        DiskIcon.classList.remove('bi-disc');
+        DiskIcon.classList.add('bi-pause-circle');
+    }else{
+        song.play();
+        DiskIcon.classList.remove('bi-pause-circle');
+        DiskIcon.classList.add('bi-disc');
+    }
+    SongOnPlay = !SongOnPlay;
+})
+EnableScrool();
+
+// SCRIPT TAMU
+const URL = new URLSearchParams(window.location.search);
+const NamaTamu = URL.get('n') || '';
+const verb = URL.get('p') || 'Bapak/Ibu/Saudara/i';
+
+document.querySelector('.hero-section main h4 span').innerText = `${verb} ${NamaTamu}`.replace(/ ,$/ ,',');
+document.querySelector('#Nama').value = NamaTamu;
