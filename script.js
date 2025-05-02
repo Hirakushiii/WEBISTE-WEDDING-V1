ba804ce9e6937a28fdb5ca260d1d7821b82aa70a
hirakushiii <rakeanajh@gmail.com>
2025-05-02 14:45:42 +0700

    FIX FEATURE

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function EnableScrool(){
     document.querySelector(":root").style.scrollBehavior = 'auto';
 }
 function DisableScrool() {
-    alert('Selamat Datang! \n Silahkan cek waktu dan tempat berlangsungnya acara \n Beserta reservasi kedatangan anda!');
+    alert('Selamat Datang!\nSilahkan cek waktu dan tempat berlangsungnya acara\nBeserta reservasi kedatangan anda');
     window.onscroll = () =>{}
     document.querySelector(":root").style.scrollBehavior = 'smooth';
     document.querySelector('#main-btn').setAttribute('href', '#home');
